a1ead721391933fc0918c2fe495bf36409456aa6
DESKTOP-O8QDTKS\rlgus <rlgus0419@gmail.com>
2022-04-06 15:27:55 +0900

    코테

---
+++ src/codingTest/test.java
@@ -0,0 +1,22 @@
+package codingTest;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String str1 = "coding" ;
+		
+		String str2 = new String("coding");
+		
+		if(str1.equals(str2)) {
+			System.out.println("값이 같지 않습니다");
+		}else {
+			System.out.println("값이 같지 않습니다");
+		}
+		
+	}
+
+}
+
+
